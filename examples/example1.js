aab6a6ff3e421dd27754eff6214f7afb0997158c
Holger-Will <h.will@klimapartner.de>
2015-10-16 20:03:38 +0200

    Update example1.js
    fixed formating

--- examples/example1.js
+++ examples/example1.js
@@ -11,13 +11,13 @@ var Button = require("../");                     // require the Button impl.
 // open the serialport in this case it's an USB Stick. the enocean pi is "/dev/ttyAMA0" and on Windows you would need something like "COM1" 
 enocean.listen("/dev/ttyUSB0");                  
 enocean.on("ready",function(base){               // when ready
-	var button = new Button(enocean,1)           // create a new Button an give it the address 1
+	var button = new Button(enocean,1)       // create a new Button an give it the address 1
 	if(process.argv[2] == "on"){
 		// if you pass the word "on" via the command line, invoke the click Event
 		// this can be use to teach in the button
-		button.A1.click()                        // if you pass the word "on" via the command line, invoke the click Event on A1
+		button.A1.click()                // if you pass the word "on" via the command line, invoke the click Event on A1
 	}else{
-		button.A0.click()	                     // if you pass the word "off" via the command line, invoke the click Event on A1
+		button.A0.click()	         // if you pass the word "off" via the command line, invoke the click Event on A1
 	}
-	enocean.close()								 // stop listening to the serial port and return
+	enocean.close()				 // stop listening to the serial port and return
 });
