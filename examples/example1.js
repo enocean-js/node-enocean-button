f53c7d2af01c787295bb1f990c91cf6b5cc26551
Holger Will <h.will@klimapartner.de>
2016-08-17 06:34:36 +0200

    make button use asynawait for click sequence (down..up)

--- examples/example1.js
+++ examples/example1.js
@@ -1,15 +1,15 @@
 // # Example of using the button
 // created By Holger Will (h.will@klimapartner.de)
-// created for [Klimapartner GmbH](http://klimapartner.de/) 
+// created for [Klimapartner GmbH](http://klimapartner.de/)
 // this code is published under GPL v3.0
 // call this with a commandline parameter. use:
 // "example1.js on" to send a click event on button A1
 // "example1.js off" to send a click event on button A0
-var enocean = require("node-enocean");           // require node-enocean
+var enocean = require("node-enocean")();           // require node-enocean
 var Button = require("../");                     // require the Button impl.
 
-// open the serialport in this case it's an USB Stick. the enocean pi is "/dev/ttyAMA0" and on Windows you would need something like "COM1" 
-enocean.listen("/dev/ttyUSB0");                  
+// open the serialport in this case it's an USB Stick. the enocean pi is "/dev/ttyAMA0" and on Windows you would need something like "COM1"
+enocean.listen("/dev/ttyUSB0");
 enocean.on("ready",function(){               // when ready
 	var button = new Button(enocean,1)       // create a new Button an give it the address 1
 	if(process.argv[2] == "on"){
--- index.js
+++ index.js
@@ -1,24 +1,26 @@
 // # A Button for node-enocean
 // created By Holger Will (h.will@klimapartner.de)
-// created for [Klimapartner GmbH](http://klimapartner.de/) 
+// created for [Klimapartner GmbH](http://klimapartner.de/)
 // this code is published under GPL v3.0
 // use this function as a constructor for a new Button Object.
 // it implements the EEP f6-02-03
 // app should be an node-enocean Object,
 // and id should be a number between 0 and 255.
-module.exports      = function ( app , id ) { 
+var async = require('asyncawait/async');
+var await = require('asyncawait/await');
+module.exports      = function ( app , id ) {
 	// the header for single Byte telegrams is allways the same for all EEPs
-	this.head       = "55000707017a" 
+	this.head       = "55000707017a"
 	// the address or senderId is the base address of the used sending device plus the id passed to this function
-	this.address    = ( parseInt( app.base , 16 ) + parseInt( id ) ).toString( 16 ) 
+	this.address    = ( parseInt( app.base , 16 ) + parseInt( id ) ).toString( 16 )
 	// this.DataByte is a little helper function to create the telegram strings for all button states
 	this.DataByte   = function ( Byte0 ) {
-		// f6 means this is an RPS telegram Byte0 is the state of the button we like to send, 
-		// this. address is the senderId 
+		// f6 means this is an RPS telegram Byte0 is the state of the button we like to send,
+		// this. address is the senderId
 		// 3001 is fixed for all RPS telegrams, ffffffff is the receiver address. all ff means this is a broudcast telegram.
 		// ff00 is signal strength and repeater count. both are fixed to ff and 00 for the send case.
 		var tmp     = "f6" + Byte0 + this.address + "3001ffffffffff00"
-		// calculating the checksum of the message body 
+		// calculating the checksum of the message body
 		tmp        += app.crc( new Buffer( tmp , "hex" ) ).toString(16)
 		// return the complete message as a string
 		return  this.head + tmp
@@ -36,61 +38,58 @@ module.exports      = function ( app , id ) {
 	this.A1downCode = this.DataByte("10") // Button A1 is pressed
 	// interface for invoking the Button events
 	this.B0 = {
-		click : function () {
+		click : async (function () {
 			// for a click event call down() and the up()
-			this.B0.down()            
-			this.B0.up()
-		}.bind( this ),
-		down  : function () {
-			// app.send expects a string representation of the enocean telegram we want to send.
-			// we have already prepared these string, we just need to send them.
-			// here we send the code indication a press event for Button 0 of channel B
-			// if our address is aabbccdd then the telegram would look like this: 55000707017af670aabbccdd3001ffffffffff0096
-			app.send(this.B0downCode) // sending the prepared string for this event
-		}.bind(this),
-		up    : function () {
-			// respectivly this is the release event of button 0 of channel B. for the EEP (f6-02-03) used here, the release telegram is the same for all 4 buttons.
-			// if you receive this code you can not tell which button was released.
-			app.send(this.B0upCode)   // sending the prepared string for this event
-		}.bind(this)
+			await (this.B0.down())
+			await (this.B0.up())
+		}.bind( this )),
+		down  : async(function () {
+			await (app.send(this.B0downCode))
+		}.bind(this)),
+		up    : async (function () {
+			await (app.send(this.B0upCode))
+		}.bind(this))
 	}
 	//  do the same for the rest of the buttons...
 	this.B1 = {
-		click : function () {
-			this.B1.down()
-			this.B1.up()
-		}.bind(this),
-		down  : function () {
-			app.send(this.B1downCode)
-		}.bind(this),
-		up    : function (){
-			app.send(this.B1upCode)
-		}.bind(this)
+		click : async (function () {
+			// for a click event call down() and the up()
+			await (this.B1.down())
+			await (this.B1.up())
+		}.bind( this )),
+		down  : async(function () {
+			await (app.send(this.B1downCode))
+		}.bind(this)),
+		up    : async (function () {
+			await (app.send(this.B1upCode))
+		}.bind(this))
 	}
 
 	this.A0 = {
-		click : function () {
-			this.A0.down()
-			this.A0.up()
-		}.bind(this),
-		down  : function () {
-			app.send(this.A0downCode)
-		}.bind(this),
-		up    : function () {
-			app.send(this.A0upCode)
-		}.bind(this)
+		click : async (function () {
+			// for a click event call down() and the up()
+			await (this.A0.down())
+			await (this.A0.up())
+		}.bind( this )),
+		down  : async(function () {
+			await (app.send(this.A0downCode))
+		}.bind(this)),
+		up    : async (function () {
+			await (app.send(this.A0upCode))
+		}.bind(this))
 	}
 
 	this.A1 = {
-		click : function () {
-			this.A1.down()
-			this.A1.up()
-		}.bind(this),
-		down  : function () {
-			app.send(this.A1downCode)
-		}.bind(this),
-		up    : function () {
-			app.send(this.A1upCode)
-		}.bind(this)
+		click : async (function () {
+			// for a click event call down() and the up()
+			await (this.A1.down())
+			await (this.A1.up())
+		}.bind( this )),
+		down  : async(function () {
+			await (app.send(this.A1downCode))
+		}.bind(this)),
+		up    : async (function () {
+			await (app.send(this.A1upCode))
+		}.bind(this))
 	}
 }
